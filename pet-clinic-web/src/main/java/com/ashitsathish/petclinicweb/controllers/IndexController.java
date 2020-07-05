c1e52f5cdd89f89effdd8744fe649d79d9234bed
ashitsathish <ashitsathish8118@gmail.com>
2020-07-05 17:56:24 -0400

    added a index controller and index html. Closes #7

---
+++ pet-clinic-web/src/main/java/com/ashitsathish/petclinicweb/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.ashitsathish.petclinicweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ashitsathish on Jul,2020
+ */
+@Controller
+public class IndexController {
+    @RequestMapping({"","/","index","index.html"})
+    public String indexPage(){
+        System.out.println("test");
+        return "index";
+    }
+}
