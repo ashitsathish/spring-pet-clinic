47501a4417a545968e725c696b7ba10b37cacb02
ashitsathish <ashitsathish8118@gmail.com>
2020-08-14 12:00:24 -0400

    added contact info details properties to Owner Class. Closes #27

--- pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Owner.java
@@ -1,18 +1,17 @@
 package com.ashitsathish.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
 /**
  * Created by ashitsathish on Jul,2020
  */
+@Getter @Setter
 public class Owner extends Person{
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
