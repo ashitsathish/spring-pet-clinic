c0c8a70902dafcd2ed2a5e5f382bcfebb8ca7f52
ashitsathish <ashitsathish8118@gmail.com>
2020-08-14 11:57:42 -0400

    added speciality class and specialities to vet. Closes #26

---
+++ pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Speciality.java
@@ -0,0 +1,12 @@
+package com.ashitsathish.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by ashitsathish on Aug,2020
+ */
+@Getter @Setter
+public class Speciality extends BaseEntity {
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Vet.java
@@ -1,7 +1,14 @@
 package com.ashitsathish.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
 /**
  * Created by ashitsathish on Jul,2020
  */
+@Getter @Setter
 public class Vet extends Person {
+    private Set<Speciality> specialities;
 }
