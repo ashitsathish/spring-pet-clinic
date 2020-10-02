9b30bb7ec344f9e06763281acc54983755423ff0
ashitsathish <ashitsathish8118@gmail.com>
2020-10-02 11:59:10 -0400

    Created Visit Entity and Set Up the relationship with pets. Closes #34

--- pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/ashitsathish/petclinic/model/Visit.java
@@ -2,14 +2,21 @@ package com.ashitsathish.petclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by ashitsathish on Aug,2020
  */
 @Data
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 }
